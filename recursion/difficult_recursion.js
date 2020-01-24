3e085bfa4baa625e143113be3121094793822aed
Pat Palmerston <patpalmerston@gmail.com>
2020-01-23 20:24:05 -0800

    recursive note md

--- recursion/difficult_recursion.js
+++ recursion/difficult_recursion.js
@@ -12,7 +12,7 @@
 
 // console.log(reverse('hello'))
 
-// PALINDROME
+// PALINDROMEe
 // - Write a recursive function called isPalindrome which returns true if the string passed to it is palindrome (reads the same forward and backward). Otherwise it returns false
 
 function isPalindrome() {}
