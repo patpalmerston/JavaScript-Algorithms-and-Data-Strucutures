15fda0627d3f6eae5ce6a20440fc3d5d08029263
Pat Palmerston <patpalmerston@gmail.com>
2020-01-30 13:49:30 -0800

    naive bubble sort

--- sorting_algos/bubbleSort.js
+++ sorting_algos/bubbleSort.js
@@ -5,3 +5,22 @@ BubbleSort Pseudocode
   - if arr[j] is greater than arr[j+1], swap those two values!
   - return the sorted array
 */
+
+// Naive Version
+
+// function swap(val1, val2) {
+//   return [val1, val2] = [val2, val1]
+// }
+
+// function bubbleSort(arr) {
+//   for(let i = 0; i < arr.length; i++) {
+//     for(let j = 0; j < arr.length; j++) {
+//       if( arr[j] > arr[j+1]) {
+//         [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+//       }
+//     }
+//   }
+//   return arr
+// }
+
+// console.log(bubbleSort([10, 17, 2, 1, 5, 4, 1, 3]))
