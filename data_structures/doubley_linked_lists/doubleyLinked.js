0bf187de6444e3de8db9866f24ffaf388c304049
Pat Palmerston <patpalmerston@gmail.com>
2020-02-06 14:59:12 -0800

    remove

--- data_structures/doubley_linked_lists/doubleyLinked.js
+++ data_structures/doubley_linked_lists/doubleyLinked.js
@@ -137,12 +137,3 @@ class DoublyLinkedList {
         return removedNode;
     }
 }
-
-let list = new DoublyLinkedList();
-list.push(0);
-list.push(1);
-list.push(2);
-list.push(3);
-list.push(4);
-list.push(5);
-list.push(6);
