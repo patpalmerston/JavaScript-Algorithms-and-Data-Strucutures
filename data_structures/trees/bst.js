0228b114bf3fce11416185b24b36c905a91f201f
Pat Palmerston <patpalmerston@gmail.com>
2020-02-07 12:19:00 -0800

    insert bst

--- data_structures/trees/bst.js
+++ data_structures/trees/bst.js
@@ -10,4 +10,42 @@ class BST {
     constructor() {
         this.root = null;
     }
+
+    insert(val) {
+        let newNode = new Node(val);
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        } else {
+            let current = this.root;
+            while (true) {
+                //exclude duplicates
+                if (val === current.val) return 'undefined';
+                if (val < current.val) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (val > current.val) {
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
 }
+
+let bst = new BST();
+bst.insert(10);
+bst.insert(5);
+bst.insert(13);
+bst.insert(11);
+bst.insert(2);
+bst.insert(16);
+bst.insert(7);
--- data_structures/trees/trees.md
+++ data_structures/trees/trees.md
@@ -26,3 +26,20 @@
     -Parent -The converse notion of a child
     -Siblings -A group of nodes with the same parent
     -Leaf -A node with no children
+
+# BST --Pseudocode--
+
+-INSERTING-
+
+-   create a new node
+-   Start at the root
+    -   Check if there is a root, if not - the root now becomes that new node
+    -   If there is a root, check if the value of the new node is greater than or less than the value of the root
+    -   If it is greater
+        -   Check to see if there is a node to the right
+            -   If there is, move to that node and repeat these steps
+            -   If there is not, add that node as the right property
+    -   If it is less
+        -   Check to see if there is a node to the left
+            -   If there is, move to that node and repeat theses steps
+            -   If there is not, add that node as the left property
