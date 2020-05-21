39d37ce9d4330b4d4bf1df86cc4b1104d2bbb003
Pat Palmerston <patpalmerston@gmail.com>
2020-05-20 17:09:43 -0700

    hash functions

--- data_structures/hash_table/hashTable.js
+++ data_structures/hash_table/hashTable.js
@@ -1,1 +1,41 @@
-console.log('hello');
+// version 1, not O1
+const hash = function (key, arrayLen) {
+    let total = 0;
+    for (let char of key) {
+        let value = char.charCodeAt(0) - 96;
+        //  console.log('value', value)
+        total = (total + value) % arrayLen;
+        // console.log('total', total)
+    }
+    return total;
+};
+// if you minus 96 from charCodeAt UTF number you will get the alphabetic reference to the letter
+
+// improving hash function, the last one was not great because
+//Only hashes strings (we wont worry about this)
+// Not constant time - linear in key length
+// Could be a little more random
+const hash2 = function (key, arrayLen) {
+    let total = 0;
+    for (let i = 0; i < key.length; i++) {
+        let char = key[i];
+        let value = char.charCodeAt(0) - 96;
+        total = (total + value) % arrayLen;
+    }
+    return total;
+};
+
+const hash3 = function (key, arrayLen) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+        let char = key[i];
+        let value = char.charCodeAt(0) - 96;
+        total = (total * WEIRD_PRIME + value) % arrayLen;
+    }
+    return total;
+};
+
+console.log(hash('pink', 10));
+console.log(hash2('pink', 10));
+console.log(hash3('hello', 13));
--- data_structures/hash_table/hash_table.md
+++ data_structures/hash_table/hash_table.md
@@ -2,3 +2,23 @@ What is a Hash Table?
 -Hash tables are used to store key-value pairs
 -They are like arrays, but the keys are not ordered
 -Unlike arrays, hash tables are fast for all of the following operations: finding values, adding new values, and removing values!
+
+Can use an array or list to implement hash table
+
+-   In order to look up values by key, we need a way to convert keys into valid array indices
+-   A function that performs this task is called a hash function
+
+Hash tables take a key:value and give you an index for where that key:value is located
+pass the 'key' to a hash function that that will give you a reference to the index at which it stores that key:value in the hashtable
+
+Hash Function
+
+-   takes any size input and outputs data of a fixed size
+-   used daily to protect data and used often in crypto
+    -Good Hash Functions
+    -Fast(constant time)
+    -Doesnt cluster outputs at specific indices, but distributes uniformly
+    -Deterministic(same input yields same output)
+    -PRIME_NUMBERS
+    -The prime number in the hash is helpful in spreading out the keys more uniformly.
+    -It's also helpful if the array that you're putting values into has a prime length.
