2303a40aa23806525d5b0391fbb5ee1f33fc336f
Pat Palmerston <patpalmerston@gmail.com>
2020-05-22 13:39:16 -0700

    graph addvertex method

--- data_structures/graphs/graphs.js
+++ data_structures/graphs/graphs.js
@@ -4,5 +4,21 @@ class Graph {
         this.adjacencyList = {};
     }
 
-    add(name) {}
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = [];
+        } else {
+            return 'Error Key already in use';
+        }
+    }
 }
+
+let g = new Graph();
+g.addVertex('Tokyo');
+g.addVertex('Tokyo');
+
+/*
+Pseudo Code for Add
+  -Write a method called addVertex, which accepts a name of a vertex
+  -It should add a key to the adjacency list with the name of the vertex and set its value to be an empty array
+*/
