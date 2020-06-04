e1c8d05be85bb913ffc120c2aef8dba866f6169f
Pat Palmerston <patpalmerston@gmail.com>
2020-06-04 16:29:05 -0700

    dijkstra pseudocode

--- dijkstra_algo/dijkstra.js
+++ dijkstra_algo/dijkstra.js
@@ -34,3 +34,22 @@ graph.addVertex('C');
 
 graph.addEdge('A', 'B', 9);
 graph.addEdge('B', 'C', 7);
+
+/*
+DIJKSTRA pseudo code
+
+-This function should accept a starting and ending vertex
+-Create an object (we'll call it distances) and set each key to be every vertex in the adjacency list with a value of infinity, except for the starting vertex which should have a value of 0.
+-After setting a value in the distances object, add each vertex with a priority of infinity to the priority queue, except the starting vertex, which should have a priority of 0 because thats where we begin
+-Create another object called previous and set each key to be every vertex in the adjacency list with a value of null
+-Start looping as long as there is anything in the priority queue
+  -dequeue a vertex from the priority queue
+  -If that vertex is the same as the ending vertex - we are done!
+  -Otherwise loop through each value in the adjacency list at that vertex
+    -Calculate the distance to that vertex from the starting vertex
+    -If the distance is less than what is currently stored in our distances object
+        -update the distances object with new lower distance 
+        -update the previous object to contain that vertex
+        -enqueue the vertex with the total distance from the start node
+
+*/
