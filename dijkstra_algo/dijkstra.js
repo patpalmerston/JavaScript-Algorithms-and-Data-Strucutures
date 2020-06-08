b79341b807fd26656695f0f834d328ee9ab93c4c
Pat Palmerston <patpalmerston@gmail.com>
2020-06-08 09:34:05 -0700

    dijkstra set up

--- dijkstra_algo/dijkstra.js
+++ dijkstra_algo/dijkstra.js
@@ -31,9 +31,18 @@ const graph = new WeightedGraph();
 graph.addVertex('A');
 graph.addVertex('B');
 graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
 
-graph.addEdge('A', 'B', 9);
-graph.addEdge('B', 'C', 7);
+graph.addEdge('A', 'B', 4);
+graph.addEdge('A', 'C', 2);
+graph.addEdge('B', 'E', 3);
+graph.addEdge('C', 'D', 2);
+graph.addEdge('C', 'F', 4);
+graph.addEdge('D', 'E', 3);
+graph.addEdge('D', 'F', 1);
+graph.addEdge('E', 'F', 1);
 
 /*
 DIJKSTRA pseudo code -
