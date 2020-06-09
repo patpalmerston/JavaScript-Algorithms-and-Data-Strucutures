88cf54e9eaa136cc1a80550a27c5738ddafb5c3d
Pat Palmerston <patpalmerston@gmail.com>
2020-06-09 11:30:10 -0700

    memoization

--- dynamic_programming/dynamic.js
+++ dynamic_programming/dynamic.js
@@ -6,3 +6,13 @@ function fib(n) {
 }
 
 console.log(fib(6));
+
+// solution with momization to optimize the repeated calls in fib recursion
+function fib2(n, memo = []) {
+    if (memo[n] !== undefined) return memo[n];
+    if (n <= 2) return 1;
+    let res = fib(n - 1, memo) + fib(n - 2, memo);
+    memo[n] = res;
+    return res;
+}
+console.log(fib2(10));
